fac30bb0582981b3fca30a61ac76cf150a67e778
angelchavez19 <infoangelchavez@gmail.com>
2024-10-09 16:03:07 -0500

    feat(auth): create login form and add CustomForm, CustomTextField, CustomSubmitButton, and CustomCallAction components

--- apps/next-app/src/app/auth/register/page.tsx
+++ apps/next-app/src/app/auth/register/page.tsx
@@ -1,139 +1,91 @@
 "use client";
 
 import React from "react";
-import {
-  Container,
-  Typography,
-  TextField,
-  Select,
-  MenuItem,
-  Button,
-  FormControl,
-  InputLabel,
-} from "@mui/material";
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material";
 import { RoleE } from "@/types/roles";
-import Link from "next/link";
+import {
+  CustomCallAction,
+  CustomForm,
+  CustomSubmitButton,
+  CustomTextField,
+} from "@/components/Form/Form";
 import { usePage } from "./page.hook";
 
 export default function AuthUserRegister() {
   const { formData, handleInputChange, handleSubmit } = usePage();
 
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        mt: 4,
-        p: 2,
-        borderRadius: 2,
-        boxShadow: 2,
-        backgroundColor: "#f5f5f5",
-      }}
-    >
-      <Typography
-        variant="h5"
-        sx={{ mb: 2, textAlign: "center", color: "#1EBAB3" }}
-      >
-        Registro de Usuario
-      </Typography>
-      <form>
-        <FormControl fullWidth sx={{ mb: 2 }}>
-          <InputLabel id="role">
-            ¿Cuál es tu rol en el proceso de adopción?
-          </InputLabel>
-          <Select
-            name="role"
-            id="role"
-            value={formData.role}
-            onChange={handleInputChange}
-            label="¿Cuál es tu rol en el proceso de adopción?"
-          >
-            <MenuItem value="" disabled>
-              Selecciona tu rol
-            </MenuItem>
-            <MenuItem value={RoleE.ADOPTER}>Adoptante</MenuItem>
-            <MenuItem value={RoleE.SHELTER}>Refugio</MenuItem>
-          </Select>
-        </FormControl>
-
-        <TextField
-          fullWidth
-          label="Nombres"
-          name="name"
-          id="name"
-          value={formData.name}
+    <CustomForm title="Registro de Usuario">
+      <FormControl fullWidth sx={{ mb: 2 }}>
+        <InputLabel id="role">
+          ¿Cuál es tu rol en el proceso de adopción?
+        </InputLabel>
+        <Select
+          name="role"
+          id="role"
+          value={formData.role}
           onChange={handleInputChange}
-          autoComplete="given-name"
-          sx={{ mb: 2 }}
-        />
-
-        <TextField
-          fullWidth
-          label="Apellidos"
-          name="lastname"
-          id="lastname"
-          value={formData.lastname}
-          onChange={handleInputChange}
-          autoComplete="family-name"
-          sx={{ mb: 2 }}
-        />
-
-        <TextField
-          fullWidth
-          label="Email"
-          type="email"
-          name="email"
-          id="email"
-          value={formData.email}
-          onChange={handleInputChange}
-          autoComplete="email"
-          sx={{ mb: 2 }}
-        />
-
-        <TextField
-          fullWidth
-          label="Número de teléfono"
-          type="tel"
-          name="phone"
-          id="phone"
-          value={formData.phone}
-          onChange={handleInputChange}
-          autoComplete="tel"
-          sx={{ mb: 2 }}
-        />
-
-        <TextField
-          fullWidth
-          label="Contraseña"
-          type="password"
-          name="password"
-          id="password"
-          value={formData.password}
-          onChange={handleInputChange}
-          sx={{ mb: 2 }}
-        />
-
-        <Button
-          variant="contained"
-          color="primary"
-          fullWidth
-          onClick={handleSubmit}
-          sx={{ mt: 2 }}
+          label="¿Cuál es tu rol en el proceso de adopción?"
         >
-          Registrar
-        </Button>
+          <MenuItem value="" disabled>
+            Selecciona tu rol
+          </MenuItem>
+          <MenuItem value={RoleE.ADOPTER}>Adoptante</MenuItem>
+          <MenuItem value={RoleE.SHELTER}>Refugio</MenuItem>
+        </Select>
+      </FormControl>
 
-        <Typography
-          variant="body2"
-          sx={{ textAlign: "center", mt: 2, color: "text.primary" }}
-        >
-          ¿Ya tienes una cuenta?{" "}
-          <Link href="/auth/login" passHref>
-            <Button variant="text" color="primary">
-              Iniciar sesión
-            </Button>
-          </Link>
-        </Typography>
-      </form>
-    </Container>
+      <CustomTextField
+        label="Nombres"
+        name="name"
+        type="text"
+        autoComplete="given-name"
+        value={formData.name}
+        onChange={handleInputChange}
+      />
+
+      <CustomTextField
+        label="Apellidos"
+        name="lastname"
+        type="text"
+        autoComplete="family-name"
+        value={formData.lastname}
+        onChange={handleInputChange}
+      />
+
+      <CustomTextField
+        label="Email"
+        name="email"
+        type="email"
+        autoComplete="email"
+        value={formData.email}
+        onChange={handleInputChange}
+      />
+
+      <CustomTextField
+        label="Número de teléfono"
+        name="phone"
+        type="tel"
+        autoComplete="tel"
+        value={formData.phone}
+        onChange={handleInputChange}
+      />
+
+      <CustomTextField
+        label="Contraseña"
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleInputChange}
+      />
+
+      <CustomSubmitButton onClick={handleSubmit} text="Registrar" />
+
+      <CustomCallAction
+        question="¿Ya tienes una cuenta?"
+        callToAction="Inicia Sesión"
+        link="/auth/login"
+      />
+    </CustomForm>
   );
 }
