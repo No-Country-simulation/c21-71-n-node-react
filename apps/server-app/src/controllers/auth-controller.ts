b0a6331e8a0bef3064df8726384d29885f791648
NovaMatt <matiasuzumy@gmail.com>
2024-10-09 21:37:31 -0300

    feat(validaciones): agregando la libreria de zod

---
+++ apps/server-app/src/controllers/auth-controller.ts
@@ -0,0 +1,52 @@
+
+import { Request, Response } from 'express';
+import bcryptjs from 'bcryptjs';
+import { INewUser, TEmailPassword } from '../../types';
+import { createUser, findUserByEmail } from '../services/user-sevice';
+import { generateToken } from '../services/jwt-service';
+
+const jwtSecret = process.env.JWT_SECRET;
+if (!jwtSecret) {
+  throw new Error('JWT_SECRET is not defined in environment variables');
+}
+
+
+
+export const userRegister = async (req: Request, res: Response) => {
+  const { email, firstname, lastname, phone, password, roleId }: INewUser =req.body;
+
+  const salt = await bcryptjs.genSalt(10);
+  const encryptedPassword = await bcryptjs.hash(password, salt);
+
+  const newUser = await createUser({email,firstname,lastname,phone,encryptedPassword,roleId})
+
+  const token = generateToken(newUser,'1h')
+
+  res.status(201).json({ ok: true, token });
+};
+
+
+
+export const userLogin = async (req: Request<TEmailPassword>, res: Response) => {
+  const { email, password } = req.body;
+  
+
+  const user = await findUserByEmail(email)
+
+  if(user){
+    
+      const isMatchPassword= await bcryptjs.compare(password,user.password)
+      if(isMatchPassword){
+        const token =  generateToken(user,'1h')
+      
+        res.status(201).json({ ok: true, token });
+      }
+      res.status(401).json({error:'invalid credentials'})
+  
+    
+  }
+
+  res.status(404).json({error:'usuario no encontrado'})
+  
+};
+
--- apps/server-app/src/routes/user-route.ts
+++ apps/server-app/src/routes/user-route.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
-import { userRegister, userLogin } from '../controllers/user-controller';
+import { userRegister, userLogin } from '../controllers/auth-controller';
 import { verifyRoleAdmin, verifyToken } from '../middlewares/verify-token-berer';
-import { getAllUsers, getUserById } from '../controllers/admin-controller';
+import { getAllUsers, getUserById, updateUserById } from '../controllers/user-controller';
 
 const router = Router();
 
@@ -15,9 +15,10 @@ router.get('/me', verifyToken, async (_req: Request, res: Response) => {
 
 
 
-// Admin
+// User
 
 router.get('/users', verifyToken, verifyRoleAdmin,getAllUsers );
 router.get('/user/:id',verifyToken, verifyRoleAdmin,getUserById)
-
+router.put('/user/:id',verifyToken,verifyRoleAdmin,updateUserById)
+router.delete('/user/:id',verifyToken,verifyRoleAdmin,)
 export default router;
