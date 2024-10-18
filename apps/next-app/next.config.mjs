38255ad986dcbd40b63c4c27f0b34fab4158959b
MaxiSiufi <maxisiufi@gmail.com>
2024-10-18 15:50:59 -0300

    chore 📦: Added Webpack image rules for optimized asset serving.

--- apps/next-app/next.config.mjs
+++ apps/next-app/next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
     images: {
       unoptimized: true, // Desactivar la optimización para evitar errores con la carga de imágenes
     },
+    webpack: (config) => {
+      config.module.rules.push({
+        test: /\.(png|jpe?g|gif|svg|webp|jfif)$/i,
+        type: 'asset/resource', // Asegura que las imágenes se sirvan correctamente
+      });
+      return config;
+    },
   };
 
 export default nextConfig;
