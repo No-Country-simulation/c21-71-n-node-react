d36d092799effe2ad8d6dc0ca438df8054b56ac4
MaxiSiufi <maxisiufi@gmail.com>
2024-10-18 14:00:00 -0300

    feat ✨: Add unoptimized image loading configuration.

--- apps/next-app/next.config.mjs
+++ apps/next-app/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+      unoptimized: true, // Desactivar la optimización para evitar errores con la carga de imágenes
+    },
   };
 
 export default nextConfig;
