a503983beec33f779bcfcb221a118fbe73b07789
MaxiSiufi <maxisiufi@gmail.com>
2024-10-18 11:03:54 -0300

    feat ✨: Enable experimental features in Next.js configuration.

--- apps/next-app/next.config.mjs
+++ apps/next-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, // Habilita el uso del Directorio App (src/app)
+    },
+  };
 
 export default nextConfig;
