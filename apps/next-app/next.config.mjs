09b9382ebba389a6e29a8be24e814fcc699873d5
MaxiSiufi <maxisiufi@gmail.com>
2024-10-18 13:03:36 -0300

    chore 📦: Update Next.js configuration and add image processing dependencies.

--- apps/next-app/next.config.mjs
+++ apps/next-app/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-      appDir: true, // Habilita el uso del Directorio App (src/app)
-    },
   };
 
 export default nextConfig;
