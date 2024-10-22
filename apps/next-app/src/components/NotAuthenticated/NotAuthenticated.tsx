84e251613f119eeb8d00e040504732c426377ca7
angelchavez19 <infoangelchavez@gmail.com>
2024-10-22 18:06:38 -0500

    feat(auth): add button to redirect unauthenticated users to login page

--- apps/next-app/src/components/NotAuthenticated/NotAuthenticated.tsx
+++ apps/next-app/src/components/NotAuthenticated/NotAuthenticated.tsx
@@ -1,18 +1,32 @@
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, Button } from "@mui/material";
 import DataDog from "@/components/Icons/datadog";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export default function NotAuthenticated() {
   const { theme } = useTheme();
 
   return (
-    <Container maxWidth="sm" sx={{ textAlign: "center", mt: 8 }}>
+    <Container maxWidth="sm" sx={{ textAlign: "center", my: 4 }}>
       <Typography variant="h4" gutterBottom>
         No autenticado
       </Typography>
-      <Box mt={4}>
+      <Box mt={4} sx={{ maxWidth: "380px", mx: "auto" }}>
         <DataDog fill={theme === "dark" ? "#ffffff" : "#000000"} />
       </Box>
+      <Link href="/auth/login" passHref>
+        <Button
+          variant="contained"
+          sx={{
+            mt: 2,
+            backgroundColor: "#1EBAB3",
+            width: "100%",
+            py: 1,
+          }}
+        >
+          Iniciar sesión
+        </Button>
+      </Link>
     </Container>
   );
 }
