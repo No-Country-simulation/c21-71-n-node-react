b1a64f044ffa6b33bfe98ea34a32cdcd8bb17f8b
NovaMatt <matiasuzumy@gmail.com>
2024-10-23 20:43:46 -0300

    fix(pet-controller) Se agrego un condicional al createPet

--- apps/server-app/src/controllers/pet-controller.ts
+++ apps/server-app/src/controllers/pet-controller.ts
@@ -4,14 +4,20 @@ import { InfoPet, UpdateInfoPet } from "../../types";
 import { MyRequest } from "../../types-back";
 import { findShelterByEmailService } from "../services/shelter-service";
 
-export const getPets = async (_req: Request, res: Response) => {
+export const getPets = async (req: MyRequest, res: Response) => {
     try {
-        const petsList = await getAllPetService()
+        const roleId=req.roleId
+        if(roleId===1){
+            const petsList = await getAllPetService()
         res.status(200).json({ ok: true, petsList })
-        return
+        
+        }else{
+            throw new Error('unauthorized')
+        }
+        
     } catch (error) {
         res.status(400).json({ ok: false, error })
-        return
+        
     }
 }
 
@@ -25,12 +31,16 @@ export const createPet = async (req: MyRequest, res: Response) => {
         
                 const newPet = await createPetService({ name, description, type, imageUrl,shelterId:shelter.id })
                 res.status(201).json({ ok: true, newPet })
+            }else{
+                res.status(404).json({ok:false, error:'shelter not found'})
             }
        
+        }else{
+            res.status(400).json({ok:false ,error:'email address not provided'})
         }
         
     } catch (error) {
-        res.status(403).json({ ok: false, error ,msg:"esta fallando algo"})
+        res.status(403).json({ ok: false, error })
     }
 }
 
@@ -43,11 +53,25 @@ export const findPetById = async (req: Request, res: Response) => {
     }
 }
 
-export const updatePet = async (req: Request, res: Response) => {
+export const updatePet = async (req: MyRequest, res: Response) => {
     try {
+        const {email,roleId}=req
+        if(email){
+        
         const { id, infoPet: { name, description, type, imageUrl } }: UpdateInfoPet = req.body
+        const findShelterIdPet=await findPetByIdService(id)
+        //findShelter?.shelterId
+        const findShelter=await findShelterByEmailService(email)
+        if(findShelterIdPet?.shelterId===findShelter?.id || roleId===1){
+            
         const changeInfoPet = await updatePetService({ id, infoPet: { name, description, type, imageUrl } })
         res.status(200).json({ ok: true, changeInfoPet })
+        }
+
+
+        }
+        
+        
     } catch (error) {
         res.status(400).json({ ok: false, error })
     }
@@ -57,7 +81,7 @@ export const deletePet = async (req: Request, res: Response) => {
     try {
         const id = Number(req.params['id'])
         const dropPet = await deletePetService(id)
-        res.status(200).json({ ok: true, message: "la Mascota ha sido borrada", dropPet })
+        res.status(200).json({ ok: true, message: "The mascot was deleted", dropPet })
     } catch (error) {
         res.status(400).json({ ok: false, error })
     }
