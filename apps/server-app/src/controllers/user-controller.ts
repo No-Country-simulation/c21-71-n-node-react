47d29b0eb8ef6b90e93c5a8da2f4131171ebb51f
NovaMatt <matiasuzumy@gmail.com>
2024-10-24 20:43:34 -0300

    refactor(user-controller) Se modifico getAllUsers

--- apps/server-app/src/controllers/user-controller.ts
+++ apps/server-app/src/controllers/user-controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from 'express';
-import { deleteUserByIdService, findUserById, getAllUsersService, updateUserByIdService } from '../services/user-sevice';
+import { deleteUserByIdService, findUserByEmail, findUserById, getAllUsersService, updateUserByEmailService, updateUserByIdService } from '../services/user-sevice';
 import { UpdateUser } from '@adopcion/types';
 
-export const getAllUsers = async (_req: Request, res: Response) => {
+export const getAllUsers = async (req: MyRequest, res: Response) => {
   try {
-    const users = await getAllUsersService();
+    if(req.roleId!==1){
+      res.status(401).json({ok:false,error:'unauthorized'})
+    }else{
+      const users = await getAllUsersService();
     res.status(200).json({ ok: true, users });
+    }
+    
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
@@ -28,6 +33,21 @@ export const updateUserById=async(req:Request,res:Response)=>{
     const {id,payload}:UpdateUser= req.body
     const user=await updateUserByIdService({id,payload})
     res.status(200).json({ok:true,user})
+    }else if(roleId===2){
+      const userEmail=req.email!
+      const findUser=await findUserByEmail(userEmail)
+      if(findUser?.email==userEmail){
+        const {payload}:UpdateUser= req.body
+        const user=await updateUserByEmailService({userEmail,payload})
+    
+       res.status(200).json({ok:true,user})
+      }else{
+        throw new Error('unauthorized')
+      }
+      
+    }
+
+    
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
